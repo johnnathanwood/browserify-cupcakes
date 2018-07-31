db6ca5e55396ad6f2b1b77735419c775b807bd30
John Wood <johnnathanwood@gmail.com>
2018-07-31 15:15:45 -0500

    Pulled new master

--- javascripts/cakeInventory.js
+++ javascripts/cakeInventory.js
@@ -1,33 +1,39 @@
+// "use strict";
+
+// const cakes = [
+//    {
+//       "description": "Marshmallow sweet roll ice cream gummi bears chocolate chocolate cake.",
+//       "flavor": "Cream",
+//       "image": "cc1.jpg",
+//       "kind": "Vanilla",
+//       "price": "12/dozen",
+//       "purchased": false,
+//       "typeId": "1"
+//    },
+//    {
+//       "description": "Sweet roll marzipan halvah ice cream cupcake tiramisu danish cotton candy cake.",
+//       "flavor": "Strawberry",
+//       "image": "cc2.jpg",
+//       "kind": "Lemon",
+//       "price": "5.00/six",
+//       "purchased": false,
+//       "typeId": "1"
+//    },
+//    {
+//       "description": "Chupa chups gummi chocolate tiramisu bonbon caramels toffee marzipan soufflé.",
+//       "flavor": "Chocolate",
+//       "image": "cc3.jpg",
+//       "kind": "Chocolate",
+//       "price": "1.00/each",
+//       "purchased": true,
+//       "typeId": "1"
+//    }
+// ];
+
+// module.exports = cakes;
+
 "use strict";
 
-const cakes = [
-   {
-      "description": "Marshmallow sweet roll ice cream gummi bears chocolate chocolate cake.",
-      "flavor": "Cream",
-      "image": "cc1.jpg",
-      "kind": "Vanilla",
-      "price": "12/dozen",
-      "purchased": false,
-      "typeId": "1"
-   },
-   {
-      "description": "Sweet roll marzipan halvah ice cream cupcake tiramisu danish cotton candy cake.",
-      "flavor": "Strawberry",
-      "image": "cc2.jpg",
-      "kind": "Lemon",
-      "price": "5.00/six",
-      "purchased": false,
-      "typeId": "1"
-   },
-   {
-      "description": "Chupa chups gummi chocolate tiramisu bonbon caramels toffee marzipan soufflé.",
-      "flavor": "Chocolate",
-      "image": "cc3.jpg",
-      "kind": "Chocolate",
-      "price": "1.00/each",
-      "purchased": true,
-      "typeId": "1"
-   }
-];
-
-module.exports = cakes;
+const watches = [
+    {}
+]
